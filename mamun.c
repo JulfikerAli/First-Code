78e71adebd094d6804c5b5cbddf56b3ead1d1136
Mamun cse <almamuncseru@gmail.com>
2018-04-26 11:19:33 +0600

    lab work

---
+++ mamun.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int main(){
+    int n,Average,Sum;
+
+    Average = Sum/n;
+
+  printf("\nSum of the %d Numbers = %d",n, Sum);
+  printf("\nAverage of the %d Numbers = %.2f",n, Average);
+
+  return 0;
+}
+
